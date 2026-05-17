f284fec55058a792d5bb0d534bcd943eb19a255b
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2026-05-17 14:42:55 +0300

    зробила  компонент Profile

---
+++ app/(private routes)/profile/page.tsx
@@ -0,0 +1,58 @@
+import css from "./ProfilePage.module.css"
+import type { Metadata } from "next";
+import Image from "next/image";
+
+
+export const metadata: Metadata = {
+  title: "Profile Page",
+  description: "User profile on NoteHub",
+  openGraph: {
+    title: "Profile Page",
+    description: "User profile on NoteHub",
+    url: "https://notehub.vercel.app/profile",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NoteHub",
+      },
+    ],
+  },
+};
+const Profile =() => {
+
+  return (
+ 
+    <main className={css.mainContent}>
+  <div className={css.profileCard}>
+      <div className={css.header}>
+	     <h1 className={css.formTitle}>Profile Page</h1>
+	     <a href="" className={css.editProfileButton}>
+	       Edit Profile
+	     </a>
+	   </div>
+     <div className={css.avatarWrapper}>
+      <Image
+        src="Avatar"
+        alt="User Avatar"
+        width={120}
+        height={120}
+        className={css.avatar}
+      />
+    </div>
+    <div className={css.profileInfo}>
+      <p>
+        Username: your_username
+      </p>
+      <p>
+        Email: your_email@example.com
+      </p>
+    </div>
+  </div>
+</main>
+  );
+  
+};
+
+export default Profile;
